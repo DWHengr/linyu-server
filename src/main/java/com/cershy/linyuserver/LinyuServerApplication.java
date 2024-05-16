e7e1711b0b9c9045bd39fd88b536fa26e0dfddb3
Heath <duweihengmr@163.com>
2024-05-16 17:36:52 +0800

    init commit

---
+++ src/main/java/com/cershy/linyuserver/LinyuServerApplication.java
@@ -0,0 +1,13 @@
+package com.cershy.linyuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinyuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinyuServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cershy/linyuserver/LinyuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cershy.linyuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinyuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
