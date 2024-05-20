ff37da0e1d0c5b8b32141d3e9f71366387a4ba51
Heath <duweihengmr@163.com>
2024-05-20 19:57:18 +0800

    config

--- src/main/java/com/cershy/linyuserver/config/MybatisHandler.java
+++ src/main/java/com/cershy/linyuserver/config/MybatisHandler.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -13,13 +12,13 @@ public class MybatisHandler implements MetaObjectHandler {
 
     @Override
     public void insertFill(MetaObject metaObject) {
-        this.setFieldValByName("createTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
-        this.setFieldValByName("updateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.setFieldValByName("updateTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
     }
 
 }
